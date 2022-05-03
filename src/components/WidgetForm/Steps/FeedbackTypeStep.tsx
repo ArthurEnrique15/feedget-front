1f78b59f7625478a1e695bfc05977ca6e95b8550
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-05-03 11:06:37 -0300

    feat: display feedback type when click on the feedback button

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,4 +1,5 @@
 import { feedbackTypes, FeedbackType } from ".."
+import { CloseButton } from "../../CloseButton";
 
 interface FeedbackTypeStepProps {
   onFeedbackTypeChanged: (type: FeedbackType) => void;
@@ -7,20 +8,28 @@ interface FeedbackTypeStepProps {
 // recebe a propriedade onFeedbackTypeChanged definida na interface FeedbackTypeStepProps
 export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProps) {
   return(
-    <div className="flex py-8 gap-2 w-full">
-      { Object.entries(feedbackTypes).map(([key, value]) => {
-        return (
-          <button
-            key={key}
-            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-            // quando o usuário clicar no botão, vai executar a função onFeedbackTypeChanged passando a key do tipo de feedback no parâmetro
-            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-          >
-            <img src={value.image.source} alt={value.image.alt} />
-            <span>{value.title}</span>
-          </button>
-        )
-      }) }
-    </div>
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        { Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              // quando o usuário clicar no botão, vai executar a função onFeedbackTypeChanged passando a key do tipo de feedback no parâmetro
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          )
+        }) }
+      </div>
+    </>
   )
 }
