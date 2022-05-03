a0409308f365624ef27c09f7bec803b193ef9bbe
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-05-03 11:05:04 -0300

    refactor: update index svg imports

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import { CloseButton } from "../CloseButton";
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
 
-import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets/idea.svg';
-import thoughtImageUrl from '../assets/thought.svg';
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
 export const feedbackTypes = {
