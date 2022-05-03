7ea8f6c94ff93a62d0d7959fad95840a48d2e1b9
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-05-03 14:10:14 -0300

    feat: display arrow left in feedback content component

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -11,6 +11,10 @@ export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) 
   return(
     <>
       <header>
+        <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
+          <ArrowLeft weight="bold" className="w-4 h-4"/>
+        </button>
+
         <span className="text-xl leading-6 flex items-center gap-2">
           <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="h-6 w-6"/>
           {feedbackTypeInfo.title}
