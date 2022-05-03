601f11b980041dd7109d98023ec25ebec80c49b3
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-05-03 15:09:41 -0300

    feat: display loading simbol while printing screen

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,6 +1,7 @@
 import html2canvas from "html2canvas";
 import { Camera } from "phosphor-react";
 import { useState } from "react";
+import { Loading } from "../Loading";
 
 export function ScreenshotButton() {
   const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
@@ -20,7 +21,8 @@ export function ScreenshotButton() {
     onClick={handleTakeScreenshot}
       className="p-2 bg-zinc-800 rounded-md border-transparent hover:gb-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
     >
-      <Camera className="w-6 h-6 text-zinc-100" />
+      { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6 text-zinc-100" /> }
+      
     </button>
   )
 }
