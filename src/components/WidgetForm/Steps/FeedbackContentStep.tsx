28ec22842e2db8813193a22fc5add9c3708a21f6
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-05-03 15:56:06 -0300

    feat: get the comment typed by the user and display it on console along with feedback type and screenshot

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -14,8 +14,16 @@ export function FeedbackContentStep({
   onRequestFeedbackRestart 
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState('')
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedback(event: FormEvent) {
+    // impede que o submit do form atualize a página (comportamento padrão do html)
+    event.preventDefault();
+    
+    console.log({ feedbackType, screenshot, comment })
+  }
 
   return(
     <>
@@ -36,10 +44,13 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <form className="my-4 w-full">
+      <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
         <textarea 
           className="min-w=[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
+          // event.target.value => texto da textarea
+          // sempre que o usuário digitar qualquer coisa na textarea, o valor da variável comment será atualizado
+          onChange={event => setComment(event.target.value)}
         />
 
         <footer className="flex gap-2 mt-2">
